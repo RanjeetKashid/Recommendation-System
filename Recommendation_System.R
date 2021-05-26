cade2acc4c7b23dcf6c483ca0b56c815bfdd0bb2
Ranjeet Kashid <63395734+RanjeetKashid@users.noreply.github.com>
2021-05-26 09:36:56 +0530

    Update Recommendation_System.R
    Deled the User ID column for the popular model and created a new data frame for the IBCF model.

--- Recommendation_System.R
+++ Recommendation_System.R
@@ -7,25 +7,37 @@ names(book)[3] <- paste("Title")
 names(book)[4] <- paste("Rating")
 str(book)
 
+sapply(book,function(x) length(unique(x)))
+
 #table(book$Title)
-table(book$User.ID)
+table_UID <- table(book$User.ID)
 table(book$Rating)
 hist(book$Rating)
 
-#book1 <- book[-(1)]
+book1 <- book[-(1)]
 
-book_matrix <- as(book, 'realRatingMatrix')
+book_matrix <- as(book1, 'realRatingMatrix')
+book2 <- arrange(book1,book1$User.ID)
+book2 <- book2[1:2000,]
+book2 <- book2[,-1]
+book_matrix1 <- as(book2,'realRatingMatrix' )
+
+### POPULAR ###
 
 book_recommend_model1 <- Recommender(book_matrix, method = "POPULAR")
 
-recommended_items1 <- predict(book_recommend_model1, book_matrix[9895:9896], n=5)
+recommended_items1 <- predict(book_recommend_model1, book_matrix[8:10], n=5)
 as(recommended_items1, "list")
 
-book_recommend_model2 <- Recommender(book_matrix, method = "IBCF")
+### IBCF ###
 
-recommended_items2 <- predict(book_recommend_model2, book_matrix[9993:9998], n=2)
+book_recommend_model2 <- Recommender(book_matrix1, method = "IBCF")
+
+recommended_items2 <- predict(book_recommend_model2, book_matrix1[8:10], n=2)
 as(recommended_items2, "list")
 
+### UBCF ###
+
 book_recommend_model3 <- Recommender(book_matrix, method = "UBCF")
-recommended_items3 <- predict(book_recommend_model3, book_matrix[100:102], n=2)
+recommended_items3 <- predict(book_recommend_model3, book_matrix[145:150], n=5)
 as(recommended_items3, "list")
